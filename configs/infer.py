cb1dc96616c312e3d9c1e50b0ef6850d9157d5c2
Akihima Tomoya <79859136+AkihimaTomoya@users.noreply.github.com>
2025-09-12 13:56:43 +0700

    Update and rename infer_config.py to infer.py

--- configs/infer.py
+++ configs/infer.py
@@ -1,22 +1,23 @@
-# configs/inference_config.py
+# configs/infer_config.py
 
+import torch
 from configs.base import config
 from pathlib import Path
 from torchvision import transforms as trans
 
-# -------------------- GHI ĐÈ CẤU HÌNH CHO INFERENCE --------------------
-
-# --- Cấu hình MẶC ĐỊNH cho model ---
 config.network = "r50"
 config.output = "work_dirs/ms1mv3_r50_onegpu"
 
-# Tên file trọng số bên trong thư mục `output`.
+config.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Embedding size
+config.embedding_size = 512
+
 config.model_file = 'model.pt'
 config.cpu_model_file = 'model.pt'
 
-# --- Các cấu hình khác cho inference ---
 config.fp16 = True
-config.threshold = 1.4
+config.threshold = 1.68
 config.tta = True
 config.update_facebank = False
 config.test_transform = trans.Compose([
